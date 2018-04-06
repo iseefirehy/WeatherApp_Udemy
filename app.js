17d921665692c2aae19f8a0cdee2aa584904f984
iseefirehy_hongyu <zhanghongyu2011@gmail.com>
2018-04-06 14:36:32 -0700

    third time

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+    .options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+       }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+    console.log(argv);
 request({
     url:'https://maps.googleapis.com/maps/api/geocode/json?address=1800%20fumia%20pl%20california',
     json: true
